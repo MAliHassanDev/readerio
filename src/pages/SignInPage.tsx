b84494e11465fdbce07d95fda1d4e787bd2342d3
ali <muhammadalihassanoffiial@gmail.com>
2025-01-25 08:47:49 +0500

    feat: add auth page templates

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router";
+
+export const SignInPage = () => {
+  return (
+    <div className="rounded-box bg-base-200 flex max-w-md flex-col gap-4 p-6">
+      <h1 className="self-center text-3xl font-bold">Log in</h1>
+
+      <span className="self-center">
+        Don't have an account?
+        <Link to="signup" className="link link-secondary">
+          Register
+        </Link>
+      </span>
+
+      <a className="btn btn-neutral">
+        <i className="fa-brands fa-google text-primary"></i>
+        Log in with Google
+      </a>
+
+      <div className="divider">OR</div>
+
+      <label className="form-control">
+        <div className="label">
+          <span className="label-text">Email</span>
+        </div>
+
+        <input className="input input-bordered" />
+      </label>
+
+      <label className="form-control">
+        <div className="label">
+          <span className="label-text">Password</span>
+          <a className="label-text link link-accent">Forgot password?</a>
+        </div>
+
+        <input type="password" className="input input-bordered" />
+      </label>
+
+      <div className="form-control">
+        <label className="label cursor-pointer gap-2 self-start">
+          <input type="checkbox" className="checkbox" />
+          <span className="label-text">Remember me</span>
+        </label>
+      </div>
+
+      <button className="btn btn-primary">Log in</button>
+    </div>
+  );
+};
