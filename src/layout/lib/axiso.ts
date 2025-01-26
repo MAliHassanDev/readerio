9e641003a88291217e24bc42d0d64629b8cbb7fd
ali <muhammadalihassanoffiial@gmail.com>
2025-01-26 10:26:07 +0500

    feat: create axios instance with config

---
+++ src/layout/lib/axiso.ts
@@ -0,0 +1,8 @@
+import Axios from "axios";
+
+export const axios = Axios.create({
+  baseURL: "http://localhost:3000/api/vi",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
