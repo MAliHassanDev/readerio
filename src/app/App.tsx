d99e78fbadfe3dd0a077bf539b42d81d55b3044c
ali <muhammadalihassanoffiial@gmail.com>
2025-01-26 10:20:26 +0500

    feat: add routes for auth pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,19 +8,29 @@ import {
   RouterProvider,
 } from "react-router";
 import { Toaster } from "react-hot-toast";
+import { SignInPage } from "@/pages/SignInPage";
+import { RegisterPage } from "@/pages/RegisterPage";
+import { AccountPage } from "@/pages/AccountPage";
+import { AccountLayout } from "@/layout/AccountLayout";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
+  createRoutesFromElements([
     <Route path={"/"} element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="write" element={<Write />} />
     </Route>,
-  ),
+
+    <Route path="/account" element={<AccountLayout />}>
+      <Route index element={<AccountPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="signin" element={<SignInPage />} />
+    </Route>,
+  ]),
 );
 
 const App = () => {
   return (
-    <div className="min-h-svh font-serif">
+    <div className="flex min-h-svh flex-col font-serif">
       <Toaster />
       <RouterProvider router={router} />
     </div>
