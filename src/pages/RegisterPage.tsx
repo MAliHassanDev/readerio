aac2f736d236aac3bb89dbaaec07518ff191989a
ali <muhammadalihassanoffiial@gmail.com>
2025-01-26 10:28:59 +0500

    feat: add useForm hook in auth forms

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,115 @@
+import { FormField } from "@/components/ui/form/FormField";
+import { useForm } from "@/hooks/useForm";
+import type { FormEvent } from "react";
+import { Form, Link } from "react-router";
+import { z } from "zod";
+
+const registerUserSchema = z
+  .object({
+    email: z
+      .string({ required_error: "Email is required" })
+      .email("Email address is invalid")
+      .or(z.literal("")),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, "Password must be between 6 and 40 characters")
+      .max(40)
+      .or(z.literal("")),
+    confirmPassword: z
+      .string({ required_error: "Confirm Password is required" })
+      .min(6, "Password must be between 6 and 40 characters.")
+      .max(40)
+      .or(z.literal("")),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+export const RegisterPage = () => {
+  const { errors, values, handleFormFieldChange, validateForm } = useForm(
+    registerUserSchema,
+    {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  );
+
+  function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    validateForm();
+  }
+
+  return (
+    <main className="flex w-full flex-1 items-center justify-center">
+      <Form
+        method="post"
+        action="/account/register"
+        onSubmit={handleFormSubmit}
+        noValidate
+      >
+        <div className="rounded-box border-light-content flex max-w-md flex-col gap-4 border p-6 shadow md:min-w-sm">
+          <h1 className="self-center text-3xl font-bold">Create an account</h1>
+
+          <span className="self-center">
+            Already have an account?
+            <Link to="../signin" className="link link-secondary">
+              Log in
+            </Link>
+          </span>
+
+          <a className="btn btn-neutral">
+            <i className="fa-brands fa-google text-primary"></i>
+            Create with Google
+          </a>
+
+          <div className="divider my-0">OR</div>
+
+          <FormField
+            name="email"
+            label="Email"
+            type="email"
+            autoComplete="email"
+            value={values.email}
+            onChange={handleFormFieldChange}
+            errorMessage={errors.email}
+          />
+
+          <FormField
+            name="password"
+            type="password"
+            label="Password"
+            value={values.password}
+            onChange={handleFormFieldChange}
+            autoComplete="new-password"
+            errorMessage={errors.password}
+          />
+
+          <FormField
+            name="confirmPassword"
+            type="password"
+            value={values.confirmPassword}
+            onChange={handleFormFieldChange}
+            label="Confirm Password"
+            autoComplete="new-password"
+            errorMessage={errors.confirmPassword}
+          />
+          <div className="form-control">
+            <label className="label cursor-pointer gap-2 self-start">
+              <input type="checkbox" className="checkbox" />
+              <span className="label-text">
+                I accept the
+                <a className="link link-accent">Terms and Conditions</a>
+              </span>
+            </label>
+          </div>
+
+          <button className="btn btn-primary" type="submit">
+            Create
+          </button>
+        </div>
+      </Form>
+    </main>
+  );
+};
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,48 +2,49 @@ import { Link } from "react-router";
 
 export const SignInPage = () => {
   return (
-    <div className="rounded-box bg-base-200 flex max-w-md flex-col gap-4 p-6">
-      <h1 className="self-center text-3xl font-bold">Log in</h1>
+    <main className="flex w-full flex-1 items-center justify-center">
+      <div className="rounded-box border-light-content flex max-w-md flex-col gap-4 border p-6 shadow md:min-w-sm">
+        <h1 className="self-center text-3xl font-bold">Log in</h1>
 
-      <span className="self-center">
-        Don't have an account?
-        <Link to="signup" className="link link-secondary">
-          Register
-        </Link>
-      </span>
+        <span className="self-center">
+          Don't have an account?
+          <Link to="../register" className="link link-secondary">
+            Register
+          </Link>
+        </span>
 
-      <a className="btn btn-neutral">
-        <i className="fa-brands fa-google text-primary"></i>
-        Log in with Google
-      </a>
+        <a className="btn btn-neutral">
+          <i className="fa-brands fa-google text-primary"></i>
+          Log in with Google
+        </a>
 
-      <div className="divider">OR</div>
+        <div className="divider">OR</div>
 
-      <label className="form-control">
-        <div className="label">
-          <span className="label-text">Email</span>
-        </div>
+        <label className="form-control">
+          <div className="label">
+            <span className="label-text block">Email</span>
+          </div>
 
-        <input className="input input-bordered" />
-      </label>
-
-      <label className="form-control">
-        <div className="label">
-          <span className="label-text">Password</span>
-          <a className="label-text link link-accent">Forgot password?</a>
-        </div>
-
-        <input type="password" className="input input-bordered" />
-      </label>
-
-      <div className="form-control">
-        <label className="label cursor-pointer gap-2 self-start">
-          <input type="checkbox" className="checkbox" />
-          <span className="label-text">Remember me</span>
+          <input className="input input-bordered" />
         </label>
-      </div>
 
-      <button className="btn btn-primary">Log in</button>
-    </div>
+        <label className="form-control">
+          <div className="label">
+            <span className="label-text">Password</span>
+          </div>
+
+          <input type="password" className="input input-bordered" />
+        </label>
+
+        <div className="form-control">
+          <label className="label cursor-pointer gap-2 self-start">
+            <input type="checkbox" className="checkbox" />
+            <span className="label-text">Remember me</span>
+          </label>
+        </div>
+
+        <button className="btn btn-primary">Log in</button>
+      </div>
+    </main>
   );
 };
